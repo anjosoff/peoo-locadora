e421ea73742335075dc2c49f64ebcd406879292b
Guilherme Oliveira dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-03-23 14:12:43 +0000

    part two (final) - change Locadora to Locacao

--- locadora/locadora.cs
+++ locadora/locadora.cs
@@ -1,6 +1,8 @@
 using System;
-public class Locadora {
+public class Locacao {
   public int IdLocacao {get;set;}
   public int IdCliente{get;set;}
   public int IdVeiculo{get;set;}  
+  public DateTime Dretira{get;set;}
+  public DateTime Ddevolve{get;set;}
 }
